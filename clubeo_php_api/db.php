a228f5a76a31560d493dacce419a1e129e8e3b0c
Guilherme-Mouro <guilhermesimoesmouro@gmail.com>
2025-11-27 21:15:54 +0000

    Initial project commit

---
+++ clubeo_php_api/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";       // ou o host que a Hostinger fornece
+$dbname = "u506280443_guimouDB";
+$username = "u506280443_guimoudbUser";
+$password = "oC5~$2OSxKJ?";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(["error" => "Database connection failed: " . $e->getMessage()]);
+    exit;
+}
+?>
