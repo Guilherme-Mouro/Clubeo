5cd7443e9ea092f0a67069bfd102f8743357b5e4
Guilherme-Mouro <guilhermesimoesmouro@gmail.com>
2025-12-03 11:29:11 +0000

    Add sidebar Menu

---
+++ clubeo_app/app/components/menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>Clubeo</h2>
+    <nav>
+        <ul>
+            <li>
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/">Discover</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/">Notifications</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+    <div>
+        <h4>My Clubs</h4>
+    </div>
+    <div>
+        <p>John Doe</p>
+    </div>
+</template>
---
+++ clubeo_app/app/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-container">
+    
+    <aside class="sidebar">
+      <Menu />
+    </aside>
+
+    <main class="content">
+      <slot />
+    </main>
+
+  </div>
+</template>
+
+<style scoped>
+.main-container {
+  display: flex;           
+  width: 100%;
+  max-width: 1200px;     
+  margin: 0 auto;          
+  min-height: 100vh;       
+}
+
+.sidebar {
+  width: 250px;         
+  flex-shrink: 0;         
+  padding: 20px;
+}
+
+.content {
+  flex-grow: 1;           
+  padding: 20px;
+}
+</style>
