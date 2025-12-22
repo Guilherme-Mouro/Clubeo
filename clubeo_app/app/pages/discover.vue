1ed725918909727b838941d3fad7bde425d7d69d
guilherme-mouro <guilhermesimoesmouro@gmail.com>
2025-12-22 19:22:15 +0000

    Fix clubs list bug

--- clubeo_app/app/pages/discover.vue
+++ clubeo_app/app/pages/discover.vue
@@ -2,7 +2,7 @@
     Discover new Clubs or create your own
     <button @click="creatClub" class="bg-custom-highlight">Create a new club</button>
 
-    <div v-for="club in clubsList" :key="club.id" class="club-card">
+    <div v-for="club in clubs" :key="club.id" class="club-card">
         <h3>{{ club.name }}</h3>
         <p>{{ club.description }}</p>
         <div class="meta">ID: {{ club.id }} | Admin ID: {{ club.admin_id }}</div>
